d49aee61a0ab8844b9315d1454f336f6e0be966d
carinesimon <contact@capability.fr>
2025-10-26 15:31:03 +0100

    Update and rename .eslintrc.js to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off', // temporaire pour débloquer le build
+    // Laisse avancer le projet : pas de blocage sur les "any"
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // Préviens seulement, et tolère les placeholders préfixés par _
     '@typescript-eslint/no-unused-vars': ['warn', {
       argsIgnorePattern: '^_',
       varsIgnorePattern: '^_',
       ignoreRestSiblings: true,
     }],
+
+    // Bon à avoir, mais en warning
     'react-hooks/exhaustive-deps': 'warn',
   },
 };
