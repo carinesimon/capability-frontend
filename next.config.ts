0a9fd3f158b79c3ade1928c45016c4a14368ed7f
carinesimon <contact@capability.fr>
2025-10-26 15:18:24 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Autorise le build même si ESLint trouve des erreurs.
+    ignoreDuringBuilds: true,
+  },
+  // Si tu as aussi des erreurs TypeScript bloquantes, décommente la ligne ci-dessous.
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
