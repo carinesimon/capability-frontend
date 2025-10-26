3175dd4186f38123c4b341e475ed90aa6ff75cc0
carinesimon <contact@capability.fr>
2025-10-26 15:35:21 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   rules: {
-    // Laisse avancer le projet : pas de blocage sur les "any"
     '@typescript-eslint/no-explicit-any': 'off',
-
-    // Préviens seulement, et tolère les placeholders préfixés par _
     '@typescript-eslint/no-unused-vars': ['warn', {
       argsIgnorePattern: '^_',
       varsIgnorePattern: '^_',
       ignoreRestSiblings: true,
     }],
-
-    // Bon à avoir, mais en warning
     'react-hooks/exhaustive-deps': 'warn',
   },
 };
